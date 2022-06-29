71ab2ff378b741b317eb4ed2e893ae1601943c13
sunil <jangraboy63123@gmail.com>
2022-06-29 23:49:55 +0530

    day1

---
+++ lib/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Catalog'),
+      ),
+      body: MaterialApp(
+        home: Material(
+          child: Center(
+            child: Container(
+              child: Text('hello'),
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
